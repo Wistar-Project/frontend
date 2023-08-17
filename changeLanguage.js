6becf16377e55f8c0932e9fca295e52cb40de8a2
fabrisdev <ezepuntocom@hotmail.com>
2023-08-16 22:33:51 -0300

    Ahora puedes actualizar tu idioma seleccionado

---
+++ changeLanguage.js
@@ -0,0 +1,12 @@
+import { setCookie } from "./utils/cookieHelper.js"
+
+const spanishButton = document.querySelector('#bandera-es')
+const englishButton = document.querySelector('#bandera-en')
+
+spanishButton.addEventListener('click', () => {
+    setCookie("language", "es", 1000)
+})
+
+englishButton.addEventListener('click', () => {
+    setCookie("language", "en", 1000)
+})
