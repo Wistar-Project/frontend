3a80cf8bb4c2a6429401eb55069a97b7093c92f9
fabrisdev <ezepuntocom@hotmail.com>
2023-08-17 03:34:36 -0300

    Banderas cambiando sin terminar

--- utils/changeLanguage.js
+++ utils/changeLanguage.js
@@ -1,15 +1,27 @@
 import { setCookie } from "./cookieHelper.js"
 import { actualizarParrafos } from "./updateText.js"
 
-const spanishButton = document.querySelector('#bandera-es')
-const englishButton = document.querySelector('#bandera-en')
+const botonBandera = document.querySelectorAll('.bandera')
 
-spanishButton.addEventListener('click', () => {
-    setCookie("language", "es", 1000)
-    actualizarParrafos()
+botonBandera.forEach(boton => {
+    boton.addEventListener('click', () => {
+        console.log(boton.getAttribute('data-language'))
+        const TIEMPO_MUY_LARGO = 1000
+        setCookie("language", boton.getAttribute('data-language'), TIEMPO_MUY_LARGO)
+        actualizarParrafos()
+        intercambiarAtributos()
+    })
 })
 
-englishButton.addEventListener('click', () => {
-    setCookie("language", "en", 1000)
-    actualizarParrafos()
-})
+function intercambiarAtributos(){
+    const ATRIBUTOS_A_INTERCAMBIAR = ["src", "alt", "id", "data-language"]
+    ATRIBUTOS_A_INTERCAMBIAR.forEach(atributo => {
+        intercambiarAtributo(atributo)
+    })
+}
+
+function intercambiarAtributo(atributo){
+    const temp = botonBandera[0][atributo]
+    botonBandera[0][atributo] = botonBandera[1][atributo]
+    botonBandera[1][atributo] = temp
+}
