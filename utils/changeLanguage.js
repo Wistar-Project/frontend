737ef6f73f5f8cbe9d4aec9ef50a676a064b059d
fabrisdev <ezepuntocom@hotmail.com>
2023-08-17 23:27:08 -0300

    Banderas no cambiando correctamente arreglado

--- utils/changeLanguage.js
+++ utils/changeLanguage.js
@@ -1,27 +1,30 @@
-import { setCookie } from "./cookieHelper.js"
+import { getCookie, setCookie } from "./cookieHelper.js"
 import { actualizarParrafos } from "./updateText.js"
 
-const botonBandera = document.querySelectorAll('.bandera')
+const banderaSiguiente = document.getElementById('bandera-siguiente')
+const banderaActual = document.getElementById('bandera-actual')
 
-botonBandera.forEach(boton => {
-    boton.addEventListener('click', () => {
-        console.log(boton.getAttribute('data-language'))
-        const TIEMPO_MUY_LARGO = 1000
-        setCookie("language", boton.getAttribute('data-language'), TIEMPO_MUY_LARGO)
-        actualizarParrafos()
-        intercambiarAtributos()
-    })
+banderaSiguiente.addEventListener('click', () => {
+    const TIEMPO_MUY_LARGO = 1000
+    setCookie("language", siguienteIdioma(), TIEMPO_MUY_LARGO)
+    actualizarParrafos()
+    intercambiarAtributos()
 })
 
+function siguienteIdioma(){
+    const idiomaActual = getCookie('language') ?? "es"
+    return idiomaActual === "es" ? "en" : "es"
+}
+
 function intercambiarAtributos(){
-    const ATRIBUTOS_A_INTERCAMBIAR = ["src", "alt", "id", "data-language"]
+    const ATRIBUTOS_A_INTERCAMBIAR = ["src", "alt"]
     ATRIBUTOS_A_INTERCAMBIAR.forEach(atributo => {
         intercambiarAtributo(atributo)
     })
 }
 
 function intercambiarAtributo(atributo){
-    const temp = botonBandera[0][atributo]
-    botonBandera[0][atributo] = botonBandera[1][atributo]
-    botonBandera[1][atributo] = temp
+    const temp = banderaActual[atributo]
+    banderaActual[atributo] = banderaSiguiente[atributo]
+    banderaSiguiente[atributo] = temp
 }
