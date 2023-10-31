e469ccd9b069389cc0d731f3bb37c86489db5417
EMILIANO AGUIRRE CASTRO <60044294@ceibal.edu.uy>
2023-10-31 17:57:28 -0300

    estructura de seguimiento de paquetes terminada y seguimiento de lotes casi pronto

--- seguimiento/paquetes/main.js
+++ seguimiento/paquetes/main.js
@@ -1,5 +1,3 @@
-import  {serverUrls} from '../../utils/consts.js'
-
 document.getElementById('boton-buscar').addEventListener('click', function(){
     const form=document.getElementById('container-buscar');
     form.classList.toggle('mostrar');
@@ -7,6 +5,8 @@ document.getElementById('boton-buscar').addEventListener('click', function(){
     const footer = document.querySelector('footer')
     const botonBuscar = document.getElementById('boton-buscar')
     const botonVolver = document.getElementById('volver')
+    const general = document.getElementById('general')
+    general.style.filter = 'grayscale(1)'
     botonVolver.style.filter = 'grayscale(1)'
     botonBuscar.style.filter = 'grayscale(1)'
     document.body.style.backdropFilter = 'grayscale(1)'
@@ -23,6 +23,8 @@ document.getElementById('cerrar').addEventListener('click' , function(){
     form.classList.toggle('mostrar');
     const botonBuscar = document.getElementById('boton-buscar')
     const botonVolver = document.getElementById('volver')
+    const general = document.getElementById('general')
+    general.style.filter = 'grayscale(0)'
     botonVolver.style.filter = 'grayscale(0)'
     botonBuscar.style.filter = 'grayscale(0)'
     document.body.style.backdropFilter = 'grayscale(0)'
@@ -31,41 +33,3 @@ document.getElementById('cerrar').addEventListener('click' , function(){
     botonBuscar.style.pointerEvents = 'all'
     botonVolver.style.pointerEvents = 'all'
 })
-document.querySelector('form')
-.addEventListener('submit', e =>{
-    e.preventDefault()
-    const data = Object.fromEntries(
-        new FormData(e.target)
-    )
-    console.log(JSON.stringify(data))
-    mostrarInformacionDeLote(data)
-})
-
-const loteInfo = document.getElementById("informacion")
-
-async function mostrarInformacionDeLote(idLote){
-    const { 
-        estado, 
-        camionAsignado, 
-        conductorAsignado, 
-        destino 
-    } = await obtenerInformacionDeLote(idLote)
-
-    loteInfo.innerHTML = `
-        <legend>Información</legend>
-        <br>
-        <p> Id del lote: ${idLote}</p>
-        <br>
-        <p> estado: ${estado}</p>
-        <br>
-        <p>Vehiculo asignado: ${camionAsignado}</p>
-        <br>
-        <p>Conductor a cargo: ${conductorAsignado}</p>
-        <br>
-        <p>Destino: ${destino}</p>
-          `
-    async function obtenerInformacionDeLote(idLote){
-        const response = await fetch(`${serverUrls.transito}/api/v1/lote/${idLote}`)
-        return await response.json()
-    }
-}
--- utils/consts.js
+++ utils/consts.js
@@ -1,10 +1,11 @@
 export const client = {
-    id: "1",
-    secret: "g2RQfGuSCkDiSgF9jNGYwZMvhgd9jmA06dmJIADC"
+    id: "3",
+    secret: "KcLHnlPwCpi0rKZWBlKNN4qukh8KO4JjZnaR8xdN"
 }
 
 export const serverUrls = {
     oauth: "http://localhost:8000",
     almacenes: "http://localhost:8001",
-    traducciones: "http://localhost:8002"
+    traducciones: "http://localhost:8002",
+    transito : "http://localhost:8003"
 }
