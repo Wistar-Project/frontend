5e01654261e2742929421ead4da7fe3aef3d118c
fabrisdev <ezepuntocom@hotmail.com>
2023-11-09 18:26:51 -0300

    adjasd

--- utils/consts.js
+++ utils/consts.js
@@ -1,12 +1,12 @@
 export const client = {
-    id: "1",
-    secret: "xAa1s5CEIsXwGuJZLgX6LvA2W9LNhviJ2gDYyPmJ"
+    id: "10",
+    secret: "UbcgNncGcibxXcaTMnSTOjuGJruTh8zv3MrixtjU"
 }
 
 export const serverUrls = {
     oauth: "http://localhost:8000",
     almacenes: "http://localhost:8001",
     traducciones: "http://localhost:8002",
-    transito : "http://localhost:8003",
+    transito: "http://localhost:8003",
     backoffice: "http://localhost:8004"
 }
