42fee5f0de249b1eeb541f44d755a81a220483d0
fabrisdev <ezepuntocom@hotmail.com>
2023-11-13 21:29:10 -0300

    Cosas no usadas removidas

--- almacen/paquetes/main.js
+++ almacen/paquetes/main.js
@@ -205,18 +205,6 @@ async function mostrarInformacionDePaquete(idPaquete){
         <p>Email: ${email}</p>
     
     `
-    let svgCode = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-  </svg>
-  `
-    const camion = document.getElementById('camion')
-  let parser = new DOMParser();
-let doc = parser.parseFromString(svgCode,"image/svg+xml");
-    camion.appendChild(doc.documentElement).setAttribute('id' , 'boton-asignar');
-    document.getElementById('boton-asignar').addEventListener('click', function(){
-        document.getElementById('container-asignar').classList.toggle('ver')
-    })
     document.getElementById('cerrar').addEventListener('click', function(){
         document.getElementById('container-asignar').classList.toggle('ver')
     })
